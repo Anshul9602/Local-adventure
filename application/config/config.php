b08dc690b5a2ee41a9ef5aa7e10110968c8848bc
Anshul9602 <anshulkumar242507@gmail.com>
2023-12-25 16:31:48 +0530

    3dn

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://localhost/3dn';
-$config['base_url'] = 'https://goodworkdaily.com/3dn';
+$config['base_url'] = 'http://localhost/3dn';
+// $config['base_url'] = 'https://goodworkdaily.com/3dn';
 
 
 // http://thecrimecontrol.in/a/
